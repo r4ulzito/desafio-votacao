b67fdbcba409739150c72bc80c9c8724725b3980
raul.santos <raul.santos@dbserver.com.br>
2023-02-24 11:16:21 -0300

    feat: estrutura inicial do projeto

---
+++ src/test/java/com/desafiovotacaoapi/desafiovotacaoapi/DesafioVotacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiovotacaoapi.desafiovotacaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
