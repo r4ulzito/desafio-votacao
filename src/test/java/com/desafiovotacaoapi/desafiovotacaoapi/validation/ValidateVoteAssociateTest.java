8424ad905af962e4d0e823c5c0e457bb6a1a4492
raul.santos <raul.santos@dbserver.com.br>
2023-03-02 18:57:23 -0300

    feat: add teste unitario ValidateVoteAssociateTest

---
+++ src/test/java/com/desafiovotacaoapi/desafiovotacaoapi/validation/ValidateVoteAssociateTest.java
@@ -0,0 +1,63 @@
+package com.desafiovotacaoapi.desafiovotacaoapi.validation;
+
+import com.desafiovotacaoapi.desafiovotacaoapi.exception.associateInvalidVoteException.AssociateInvalidVoteException;
+import com.desafiovotacaoapi.desafiovotacaoapi.model.Associate;
+import com.desafiovotacaoapi.desafiovotacaoapi.model.Topic;
+import com.desafiovotacaoapi.desafiovotacaoapi.model.Vote;
+import com.desafiovotacaoapi.desafiovotacaoapi.model.enums.Answer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateVoteAssociateTest {
+
+    @Test
+    @DisplayName("Deve lançar uma exceção caso o associado ja tenha um voto na lista de votos referente ao topico")
+    public void validateAssociateCanVoteTest() {
+
+        Associate fakeAssociate = new Associate(1L, "Associate1");
+        Topic fakeTopic = new Topic(1L, "Title1", "Description1");
+
+        List<Vote> voteList = new ArrayList<>();
+        voteList.add(new Vote(1L, fakeAssociate, fakeTopic, Answer.YES));
+
+        String errorMessage = "";
+
+        try {
+            ValidateVoteAssociate.validateAssociateCanVote(voteList, fakeAssociate);
+
+        } catch (AssociateInvalidVoteException ex) {
+            errorMessage = ex.getMessage();
+        }
+
+        assertEquals(errorMessage, "Associate already voted for this topic!");
+
+    }
+
+    @Test
+    @DisplayName("Nao deve fazer nada caso o associado nao tenha um voto na lista de votos referente ao topico")
+    public void validateAssociateCanVoteWithValidAssociateTest() {
+
+        Associate fakeAssociate = new Associate(1L, "Associate1");
+        Topic fakeTopic = new Topic(1L, "Title1", "Description1");
+
+        List<Vote> voteList = new ArrayList<>();
+        voteList.add(new Vote(1L, new Associate(), fakeTopic, Answer.YES));
+
+        String errorMessage = "";
+
+        try {
+            ValidateVoteAssociate.validateAssociateCanVote(voteList, fakeAssociate);
+        } catch (AssociateInvalidVoteException ex) {
+            errorMessage = ex.getMessage();
+        }
+
+        assertEquals(errorMessage, "");
+
+    }
+
+}
