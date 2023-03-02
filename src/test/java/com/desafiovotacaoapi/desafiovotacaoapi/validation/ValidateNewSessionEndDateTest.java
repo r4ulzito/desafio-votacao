bf2ef59ccccccdaa835ba299e6ac4edaf14c955b
raul.santos <raul.santos@dbserver.com.br>
2023-03-02 18:42:39 -0300

    feat: add testes unitarios ValidateNewSessionEndDateTest e ValidateQueryIsNull

---
+++ src/test/java/com/desafiovotacaoapi/desafiovotacaoapi/validation/ValidateNewSessionEndDateTest.java
@@ -0,0 +1,53 @@
+package com.desafiovotacaoapi.desafiovotacaoapi.validation;
+
+import com.desafiovotacaoapi.desafiovotacaoapi.exception.invalidDateEndException.InvalidDateEndException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateNewSessionEndDateTest {
+
+    @Test
+    @DisplayName("Deve retornar o endDate sem alterações")
+    public void validateEndDateWithValidEndDateTest() {
+
+        LocalDateTime fakeEndDate = LocalDateTime.now().plusHours(1).withNano(0);
+
+        LocalDateTime validatedEndDate = ValidateNewSessionEndDate.validateEndDate(fakeEndDate);
+
+        assertEquals(validatedEndDate, fakeEndDate);
+
+    }
+
+    @Test
+    @DisplayName("Deve retornar o endDate com 1 minuto a mais caso seja nulo")
+    public void validateEndDateWithNullEndDateTest() {
+
+        LocalDateTime now = LocalDateTime.now().withNano(0);
+
+        LocalDateTime validatedEndDate = ValidateNewSessionEndDate.validateEndDate(null);
+
+        assertEquals(ChronoUnit.MINUTES.between(now, validatedEndDate.withNano(0)), 1);
+
+    }
+
+    @Test
+    @DisplayName("Deve lançar uma exceção caso o endDate seja menor que o localDateTime atual")
+    public void validateEndDateWithInvalidEndDateTest() {
+
+        LocalDateTime fakeEndDate = LocalDateTime.now().minusHours(1).withNano(0);
+
+        try {
+            LocalDateTime validatedEndDate = ValidateNewSessionEndDate.validateEndDate(fakeEndDate);
+            assertNull(validatedEndDate);
+        } catch (InvalidDateEndException ex) {
+            assertEquals(ex.getMessage(), "Invalid session ends date!");
+        }
+
+    }
+
+}
---
+++ src/test/java/com/desafiovotacaoapi/desafiovotacaoapi/validation/ValidateQueryIsNullTest.java
@@ -0,0 +1,41 @@
+package com.desafiovotacaoapi.desafiovotacaoapi.validation;
+
+import com.desafiovotacaoapi.desafiovotacaoapi.exception.nullQueryResultException.NullQueryResultExcepetion;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateQueryIsNullTest {
+
+    @Test
+    @DisplayName("Deve lançar uma exeção caso o resultado da pesquisa validado seja nulo")
+    public void queryIsNullWithNullQueryResultTest() {
+
+        Optional queryResultFake = Optional.empty();
+
+        try {
+            ValidateQueryIsNull.queryIsNull(queryResultFake, "Query is null!");
+        } catch (NullQueryResultExcepetion ex) {
+            assertEquals(ex.getMessage(), "Query is null!");
+        }
+
+    }
+
+    @Test
+    @DisplayName("Não deve fazer nada caso o resultado da pesquisa seja diferente de nulo")
+    public void queryIsNullWithNotNullQueryResultTest() {
+
+        Optional<Integer> queryResultFake = Optional.of(1);
+
+        try {
+            ValidateQueryIsNull.queryIsNull(queryResultFake, "Query is null!");
+        } catch (NullQueryResultExcepetion ex) {
+            assertNull(ex);
+        }
+
+    }
+
+}
